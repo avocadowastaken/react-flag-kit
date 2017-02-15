dc47c825a033c26b2496795d706af8a19a128f2f
Umidbek Karimov <uma.karimov@gmail.com>
2017-02-15 19:24:57 +0400

    Update `deploy-docs.sh` script.

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -2,20 +2,15 @@
 
 set -o errexit -o nounset
 
-rev=$(git rev-parse --short HEAD)
-
 cd docs
 
 git init
 git config user.name "Umidbek Karimov"
 git config user.email "uma.karimov@gmail.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/umidbekkarimov/react-flag-kit.git"
-git fetch upstream
-git reset upstream/gh-pages
+git add .
+git commit -m "Deploy Docs to GitHub Pages" > /dev/null
 
-touch .
+git push --force --quiet "https://$GH_TOKEN@github.com/umidbekkarimov/react-flag-kit.git" master:gh-pages
 
-git add -A .
-git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+cd ..
